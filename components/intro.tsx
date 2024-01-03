133b4c82716499f1594ce79e965b13701fe8ac78
Kapil Dev <talktokapildev@gmail.com>
2024-01-03 16:42:55 +0000

    eslint error

--- components/intro.tsx
+++ components/intro.tsx
@@ -71,7 +71,7 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Kapil.</span> I'm a{" "}
+        <span className="font-bold">Hello, I am Kapil.</span> I am a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">15 years</span> of total experience. I enjoy
         building <span className="italic">sites</span>. My focus is{" "}
