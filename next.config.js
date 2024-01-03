6a39a4ddb5aee5175d558e8be9b0290a35dedf58
Kapil Dev <talktokapildev@gmail.com>
2024-01-03 16:32:55 +0000

    portfolio initial

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },{
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
 
 module.exports = nextConfig
